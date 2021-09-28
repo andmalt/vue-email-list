1ecef3678196c6113eb91cc6d64420a86027dca3
andmalt <andrea.maltempi@gmail.com>
2021-09-28 16:28:32 +0200

    inizialized exercise

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
+import Axios from 'axios'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Main,
+  },
+  Mounted(){
+    console.log(Axios);
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="main-content">
+      <ul>
+        <li></li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Main',
+}
+</script>
+
+
+<style scoped lang="scss">
+.main-content{
+  display: flex;
+  & ul{
+    list-style: none;
+  }
+}
+</style>
